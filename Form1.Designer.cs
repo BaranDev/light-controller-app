c76e40c363ace518c3f5eaf7cb2720ab82157926
baran <mrcevdetbaran@hotmail.com>
2023-05-07 03:16:13 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace light_controller_app
+{
+    partial class LightController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlMenuBar = new System.Windows.Forms.Panel();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblCurrentMenu = new System.Windows.Forms.Label();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.pnlNav = new System.Windows.Forms.Panel();
+            this.btnRooms = new System.Windows.Forms.Button();
+            this.btnDevices = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pnlMenuBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.pnlNav.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMenuBar
+            // 
+            this.pnlMenuBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.pnlMenuBar.Controls.Add(this.btnMin);
+            this.pnlMenuBar.Controls.Add(this.btnExit);
+            this.pnlMenuBar.Controls.Add(this.lblCurrentMenu);
+            this.pnlMenuBar.Controls.Add(this.pbLogo);
+            this.pnlMenuBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlMenuBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenuBar.Name = "pnlMenuBar";
+            this.pnlMenuBar.Size = new System.Drawing.Size(804, 40);
+            this.pnlMenuBar.TabIndex = 0;
+            this.pnlMenuBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlMenuBar_MouseDown);
+            // 
+            // btnMin
+            // 
+            this.btnMin.BackgroundImage = global::light_controller_app.Properties.Resources.btnMin_42;
+            this.btnMin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMin.FlatAppearance.BorderSize = 0;
+            this.btnMin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.btnMin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.btnMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMin.Location = new System.Drawing.Point(756, 10);
+            this.btnMin.Margin = new System.Windows.Forms.Padding(10);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(20, 20);
+            this.btnMin.TabIndex = 0;
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackgroundImage = global::light_controller_app.Properties.Resources.btnExit_41;
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(781, 10);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(10);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(20, 20);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblCurrentMenu
+            // 
+            this.lblCurrentMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblCurrentMenu.Font = new System.Drawing.Font("Konkhmer Sleokchher", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentMenu.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(120)))), ((int)(((byte)(141)))));
+            this.lblCurrentMenu.Location = new System.Drawing.Point(161, 0);
+            this.lblCurrentMenu.Name = "lblCurrentMenu";
+            this.lblCurrentMenu.Size = new System.Drawing.Size(643, 40);
+            this.lblCurrentMenu.TabIndex = 3;
+            this.lblCurrentMenu.Text = "Dashboard";
+            this.lblCurrentMenu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCurrentMenu.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblCurrentMenu_MouseDown);
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.BackgroundImage = global::light_controller_app.Properties.Resources.barandev_logo_41;
+            this.pbLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbLogo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbLogo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pbLogo.Location = new System.Drawing.Point(0, 0);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(161, 40);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbLogo.TabIndex = 3;
+            this.pbLogo.TabStop = false;
+            this.pbLogo.Tag = "";
+            this.toolTip1.SetToolTip(this.pbLogo, "github.com/barandev");
+            this.pbLogo.Click += new System.EventHandler(this.pbLogo_Click);
+            // 
+            // pnlNav
+            // 
+            this.pnlNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.pnlNav.Controls.Add(this.btnRooms);
+            this.pnlNav.Controls.Add(this.btnDevices);
+            this.pnlNav.Controls.Add(this.btnHome);
+            this.pnlNav.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlNav.Location = new System.Drawing.Point(0, 380);
+            this.pnlNav.Name = "pnlNav";
+            this.pnlNav.Size = new System.Drawing.Size(804, 81);
+            this.pnlNav.TabIndex = 2;
+            // 
+            // btnRooms
+            // 
+            this.btnRooms.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnRooms.FlatAppearance.BorderSize = 0;
+            this.btnRooms.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRooms.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRooms.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(120)))), ((int)(((byte)(141)))));
+            this.btnRooms.Image = global::light_controller_app.Properties.Resources.rooms_;
+            this.btnRooms.Location = new System.Drawing.Point(336, 0);
+            this.btnRooms.Name = "btnRooms";
+            this.btnRooms.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.btnRooms.Size = new System.Drawing.Size(161, 81);
+            this.btnRooms.TabIndex = 3;
+            this.btnRooms.Text = "Rooms";
+            this.btnRooms.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnRooms.UseVisualStyleBackColor = true;
+            // 
+            // btnDevices
+            // 
+            this.btnDevices.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDevices.FlatAppearance.BorderSize = 0;
+            this.btnDevices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDevices.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDevices.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(120)))), ((int)(((byte)(141)))));
+            this.btnDevices.Image = global::light_controller_app.Properties.Resources.bulb;
+            this.btnDevices.Location = new System.Drawing.Point(169, 0);
+            this.btnDevices.Name = "btnDevices";
+            this.btnDevices.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.btnDevices.Size = new System.Drawing.Size(161, 81);
+            this.btnDevices.TabIndex = 3;
+            this.btnDevices.Text = "Devices";
+            this.btnDevices.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnDevices.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(120)))), ((int)(((byte)(141)))));
+            this.btnHome.Image = global::light_controller_app.Properties.Resources.home__;
+            this.btnHome.Location = new System.Drawing.Point(0, 0);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.btnHome.Size = new System.Drawing.Size(161, 81);
+            this.btnHome.TabIndex = 3;
+            this.btnHome.Text = "Dashboard";
+            this.btnHome.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Location = new System.Drawing.Point(0, 40);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Size = new System.Drawing.Size(804, 340);
+            this.pnlContent.TabIndex = 3;
+            // 
+            // LightController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(17)))), ((int)(((byte)(32)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(804, 461);
+            this.Controls.Add(this.pnlContent);
+            this.Controls.Add(this.pnlNav);
+            this.Controls.Add(this.pnlMenuBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LightController";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Light Controller";
+            this.pnlMenuBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.pnlNav.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMenuBar;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Panel pnlNav;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblCurrentMenu;
+        private System.Windows.Forms.Button btnRooms;
+        private System.Windows.Forms.Button btnDevices;
+        private System.Windows.Forms.Panel pnlContent;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
